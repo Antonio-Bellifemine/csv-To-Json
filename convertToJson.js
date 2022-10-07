8421ee27f1e34e35c015bbcb11b41e81f19a6928
Antonio Bellifemine <antonio@trustlayer.io>
2022-10-07 14:30:07 -0400

    change name of json file

--- convertToJson.js
+++ convertToJson.js
@@ -6,11 +6,15 @@ csv()
   .then((jsonObj) => {
     jsonObj;
 
-    fs.writeFile("jsonPolicies.json", JSON.stringify(jsonObj), (err) => {
-      if (err) throw err;
-      console.log("Here is the error we encountered:", err);
-      console.log("csv file has been converted to json successfully");
-    });
+    fs.writeFile(
+      "nameOfJsonFileToOutput.json",
+      JSON.stringify(jsonObj),
+      (err) => {
+        if (err) throw err;
+        console.log("Here is the error we encountered:", err);
+        console.log("csv file has been converted to json successfully");
+      }
+    );
   });
 
 // csvToJson.fieldDelimiter(";").getJsonFromCsv(fileInputName);
